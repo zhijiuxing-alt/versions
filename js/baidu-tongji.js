e4e554164e87f269437327e624e3a8541c8b6d90
zhijiuxing-alt <zhijiuxing-alt@users.noreply.github.com>
2021-07-21 15:45:11 +0000

    deploy: 72f4edfabf6519d54b620050ac35aa01c8eefa2d

--- js/baidu-tongji.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ecca3fe024165945ea426dba5a846e29";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
